393c5298ce5a244da8464fee0f16875196653c0b
taylorsieling <taylorasieling@gmail.com>
2021-08-03 17:07:32 -0500

    setting up header file

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WP Hierarchy</title>
-</head>
-<body>
+<?php get_header(); ?>
 
     <h1>WP Hierarchy</h1>    
     
